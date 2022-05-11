384e02ea53e4072644714af05dc79e3e02a0d2ea
Hilman F. Rakhmad <hifraofficial@gmail.com>
2022-05-11 16:54:42 +0700

    add beforeConnect hook to sequelize

--- src/config/db.js
+++ src/config/db.js
@@ -1,39 +1,24 @@
 const { Sequelize } = require('sequelize')
 const Vault = require('../utils/vault');
 
-const vault = new Vault(process.env.VAULT_URL, process.env.JWT_PATH);
-
-let db_name;
-let db_user;
-let db_password;
-
-const fetchConfig = async () => {
+const sequelize = new Sequelize(
+    config.database,
+    '',
+    '',
+    config
+)
+sequelize.beforeConnect(async (config) => {
+    const vault = new Vault(process.env.VAULT_URL, process.env.JWT_PATH);
     await vault.login();
-    const config = await vault.getConfig(process.env.CONFIG_PATH);
+    const dbConfig = await vault.getConfig(process.env.CONFIG_PATH);
+    console.log(dbConfig)
+    
+    config.database = dbConfig.db_name;
+    config.username = dbConfig.db_user;
+    config.password = dbConfig.db_password;
+    config.host = process.env.DB_HOST;
+    config.dialect = 'mysql';
+    config.logging = process.env.NODE_ENV === 'production' ? false : console.log;
+})
 
-    console.log(config);
-
-    db_name = config.db_name;
-    db_user = config.db_user;
-    db_password = config.db_password;
-}
-
-const initSequelize = () => {
-    let sequelize;
-    fetchConfig().then(() => {
-        sequelize = new Sequelize(
-            db_name,
-            db_user,
-            db_password,
-            {
-                host: process.env.DB_HOST,
-                dialect: 'mysql',
-                logging: process.env.NODE_ENV === 'production' ? false : console.log
-            }
-        )
-    })
-
-    return sequelize
-}
-
-module.exports = initSequelize();
+module.exports = sequelize;
