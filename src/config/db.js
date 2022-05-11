8f59c213a64429caa7af5b400ab0bd828ee95fbb
Hilman F. Rakhmad <hifraofficial@gmail.com>
2022-05-11 13:33:28 +0700

    retrieve config from hashicorp vault

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,35 @@
 const { Sequelize } = require('sequelize')
+const Vault = require('../utils/vault');
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        dialect: 'mysql',
-        logging: process.env.NODE_ENV === 'production' ? false : console.log
-    }
-)
+const vault = new Vault(process.env.VAULT_URL, process.env.JWT_PATH);
+
+let db_name;
+let db_user;
+let db_password;
+let sequelize;
+
+const fetchConfig = async () => {
+    await vault.login();
+    const config = await vault.getConfig(process.env.CONFIG_PATH);
+
+    console.log(config);
+
+    db_name = config.db_name;
+    db_user = config.db_user;
+    db_password = config.db_password;
+}
+
+fetchConfig().then(() => {
+    sequelize = new Sequelize(
+        db_name,
+        db_user,
+        db_password,
+        {
+            host: process.env.DB_HOST,
+            dialect: 'mysql',
+            logging: process.env.NODE_ENV === 'production' ? false : console.log
+        }
+    )
+})
 
 module.exports = sequelize
---
+++ src/utils/vault.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+
+class Vault {
+
+    secretApi = '/v1/secret/';
+    kubernetesApi = '/v1/auth/kubernetes/login';
+    vaultToken = '';
+
+    constructor(vaultUrl, jwtPath) {
+        this.vaultUrl = vaultUrl;
+        this.jwtPath = jwtPath;
+        this.token = fs.readFileSync(this.jwtPath, 'utf-8');
+    }
+
+    async login() {
+        const response = await fetch(this.vaultUrl + this.kubernetesApi, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                role: "todoapp-backend",
+                jwt: this.token
+            })
+        })
+
+        const responseJson = await response.json();
+
+        this.vaultToken = responseJson.auth.client_token;
+    }
+
+    async getConfig(configPath) {
+        const response = await fetch(this.vaultUrl + /v1/ + configPath, {
+            headers: {
+                'X-Vault-Token': this.vaultToken
+            },
+        })
+
+        const responseJson = await response.json();
+
+        return responseJson.data;
+    }
+}
+
+module.exports = Vault;
