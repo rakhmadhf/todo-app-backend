7ea6677a5fcd15ee3e5cf02b8a53559e277a1950
Hilman F. Rakhmad <hifraofficial@gmail.com>
2022-05-17 16:43:41 +0700

    secret file path

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config({path: process.env.SECRET_FILE_PATH})
 const express = require('express')
 const cors = require('cors')
 const api = require('./api/routes')
