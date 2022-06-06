7c6c49baff1e215be420a775ab712e7cda7a3c40
Hilman F. Rakhmad <hifraofficial@gmail.com>
2022-06-06 14:48:25 +0700

    api metrics

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ const express = require('express')
 const cors = require('cors')
 const api = require('./api/routes')
 const syncDb = require('./utils/syncDb')
+const apiMetrics = require('prometheus-api-metrics')
 
 const PORT = 3000
 
@@ -12,6 +13,8 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+app.use(apiMetrics())
 
 app.use('/api/v1', api)
 
